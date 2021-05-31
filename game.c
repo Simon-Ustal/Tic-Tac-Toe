f8d7b08968fba06ad15f34adf2ae2cfc28f3df4b
Simon-Ustal <67018424+Simon-Ustal@users.noreply.github.com>
2021-05-31 07:40:04 +0200

    Initial commit

---
+++ game.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "game.h"
+
+struct EnemyLoc
+{
+    int EnemyLocationX;
+    int EnemyLocationY; 
+};
+
+void sleep(unsigned int mseconds)
+{
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+    return;
+}
+
+char doMove(char playerChar, int positionX, int positionY, int numOfPoles, char poles[][numOfPoles]){
+    poles[positionX - 1 ][positionY - 1] = playerChar;
+        for (int i = 0; i < numOfPoles; i++){
+         for(int j = 0; j < numOfPoles; j++){
+            printf("%c ", poles[i][j]);
+        }
+    printf("\n");
+        }
+return 1;
+}
+
+int RandGenNum(int numOfPoles){
+int upperX = numOfPoles - 1;
+int lowerX = 0;   
+int numX = (rand() % (upperX - lowerX + 1)) + lowerX;
+return numX;
+}
+
+void enemyPlayLoad(){
+system("cls");
+printf("Nyni hraje souper.\n");
+sleep(750);
+system("cls");
+printf("Nyni hraje souper..\n");
+sleep(750);
+system("cls");
+printf("Nyni hraje souper...\n");
+sleep(750);
+system("cls");
+return;
+}
+
+struct EnemyLoc AIMove(int positionX, int positionY, int numOfPoles, char poles[][numOfPoles]){
+    int numberX = RandGenNum(numOfPoles);
+    int numberY = RandGenNum(numOfPoles);
+
+    struct EnemyLoc s1;
+
+//Generating of X position
+    int enemyXPosition = (positionX - numberX) / 2;
+
+    if(enemyXPosition < 1){
+    enemyXPosition = 1;
+    enemyXPosition = enemyXPosition + numberX;
+    } else if (enemyXPosition > numOfPoles - 1){
+    enemyXPosition = numOfPoles;
+    enemyXPosition = enemyXPosition - numberY;
+    }
+
+    for (int i = 0; i < numOfPoles; i++){
+    for (int f = 0; i < numOfPoles; i++){ 
+        if (poles[i][f] == 'x' || poles[i][f] == 'o'){
+            enemyXPosition == enemyXPosition + 1;
+            printf("Posun z X\n");
+        }
+    }        
+}
+
+//Generating of Y position
+    int enemyYPosition = (numberY - numberY) / 2;
+
+    if(enemyYPosition < 1){
+    enemyYPosition = 1;
+    enemyYPosition = enemyYPosition + numberY;
+    } else if (enemyYPosition > numOfPoles - 1){
+    enemyYPosition = numOfPoles;
+    enemyYPosition = enemyYPosition - numberY;
+    }
+
+for (int i = 0; i < numOfPoles ; i++){
+    for (int f = 0; i < numOfPoles; i++){ 
+        if (poles[i][f] == 'x' || poles[i][f] == 'o'){
+            enemyYPosition == enemyYPosition + 1;
+            printf("Posun z Y\n");
+        }
+    }       
+}
+
+    s1.EnemyLocationX = enemyXPosition;
+    s1.EnemyLocationY = enemyYPosition;
+
+    printf("osa X: %d\n", enemyXPosition);
+    printf("osa Y: %d\n", enemyYPosition);
+
+    return s1;
+}
+
+void game(void){
+
+    int numberOfPoles, positionX, positionY, enemyPositionX = 1, enemyPositionY = 1;
+    char playerChar, enemyChar;
+
+    system("cls");
+
+    while(playerChar != 'x' && playerChar != 'o'){
+        printf("Za jaky znak chcete hrat? (x - o) \n"); // Vyber znaku
+        printf("Chci hrat za: ");
+        scanf("%c", &playerChar);
+        system("cls");
+        }
+
+    if(playerChar == 'x'){
+    enemyChar = 'o'; 
+    } else{
+    enemyChar = 'x';    
+    }
+
+
+    while(numberOfPoles < 3 || numberOfPoles > 5 ){
+        printf("Na kolik poli chcete hrat?\n");
+        printf("Chci hrat na: ");
+        scanf("%d", &numberOfPoles);
+        system("cls");
+    }
+        
+    char generatedPoles [numberOfPoles][numberOfPoles];
+
+fflush(stdin);
+
+        system("cls");
+
+        for (int i = 0; i < numberOfPoles; i++){
+        for(int j = 0; j < numberOfPoles; j++){
+        generatedPoles [i][j] = '-';
+        printf("%c ", generatedPoles[i][j]);
+    }
+    printf("\n");
+}
+    while(1){
+        printf("Na jakou pozici po ose X chcete umistit znak: ");
+        scanf("%d", &positionX);
+
+        printf("Na jakou pozici po ose Y chcete umistit znak: ");
+        scanf("%d", &positionY);
+
+        system("cls");
+
+
+        doMove(playerChar, positionX, positionY, numberOfPoles, generatedPoles);
+
+        enemyPlayLoad();
+
+        struct EnemyLoc s1 =  AIMove(positionX, positionY, numberOfPoles, generatedPoles);
+
+        enemyPositionX = s1.EnemyLocationX;
+        enemyPositionY = s1.EnemyLocationY;
+
+        doMove(enemyChar, enemyPositionX , enemyPositionY, numberOfPoles, generatedPoles);
+    }
+
+return;
+}
---
+++ main.c
@@ -0,0 +1,27 @@
+// Šimon Ustal II.B xx.6.2021 ========== Tic-Tac-Toe ==========
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "game.h"
+
+int main(void) {
+  char indicator;
+    printf("\n");
+    printf("========== Tic-Tac-Toe ==========\n\n");
+    printf("Pro spusteni hry stisknete [Num 1]\n");
+    printf("Pro ukonceni hry stisknete [Num 2]\n\n");
+    printf("Vybrany znak: ");
+    scanf("%c", &indicator);
+
+  fflush(stdin);
+
+  if(indicator == '1'){
+  game();
+  }
+
+  else if(indicator == '2') {
+  return 0;
+  }
+
+}
